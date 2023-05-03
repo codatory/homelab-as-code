36c6d572bd2ac548431016ad1c3567e140be69ba
Alex Conner <hello@codatory.com>
2023-05-03 10:56:38 -0400

    Add chart: node problem detector

---
+++ Kubernetes/Charts/node-problem-detector/install.sh
@@ -0,0 +1,6 @@
+helm repo add deliveryhero https://charts.deliveryhero.io/
+helm repo update deliveryhero
+helm upgrade --install --wait-for-jobs --create-namespace \
+      --namespace kube-system node-problem-detector \
+      deliveryhero/node-problem-detector \
+      --set metrics.enabled=true
