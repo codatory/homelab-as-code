3332cd03e128b8b15d49c37b3c3b31f8cacd6dc4
Alex Conner <hello@codatory.com>
2023-05-04 11:36:48 -0400

    Add chart: gitea w/ pg operator

---
+++ Kubernetes/Charts/gitea/install.sh
@@ -0,0 +1,7 @@
+helm repo add gitea https://dl.gitea.io/charts/
+helm repo update gitea
+
+helm upgrade --install --values values.yaml --namespace gitea --create-namespace \
+   gitea gitea/gitea
+
+kubectl apply -f database.yaml --namespace gitea
